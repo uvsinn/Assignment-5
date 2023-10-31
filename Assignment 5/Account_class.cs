fd7c161ddd3a1ccd2b27b177511ea20ef9fe7a9f
u31d64 <u31d64@INGURWN110893>
2023-10-31 17:24:33 +0530

    Add project files.

---
+++ Assignment 5/Account_class.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_5
+{
+    internal class Account
+    {
+        int id;
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+        string? accountType;
+        public string? AccountType
+        {
+            get
+            {
+                return accountType;
+            }
+            set
+            {
+                accountType = value;
+            }
+        }
+        double balance;
+        public double Balance
+        {
+            get
+            {
+                return balance;
+            }
+            set
+            {
+                balance = value;
+            }
+        }
+
+        public bool Withdraw(int amount)
+        {
+            if (balance < amount)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public string GetDetails()
+        {
+            string message = "";
+            message += $"Account Id : {id}";
+            message += $"Account Type: {accountType}";
+            message += $"Balance: {balance}";
+            return message;
+        }
+    }
+}
---
+++ Assignment 5/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualBasic;
+using System.Security.Principal;
+
+namespace Assignment_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account();
+
+            Console.WriteLine("Enter account id\n");
+            account.Id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter account Type\n");
+            account.AccountType = Console.ReadLine();
+            Console.WriteLine("Enter accont balance\n");
+            account.Balance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter amount to withdraw\n");
+            int withdrawn_amount = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(account.GetDetails());
+            //int id;
+            //string accType;
+            //double balance;
+            //account.GetDetails(out id, out accType, out balance);
+            //Console.WriteLine($"Account Id: {id} \n Account Type: {accType}\n Balance: {balance}");
+            if (account.Withdraw(withdrawn_amount))
+            {
+                Console.WriteLine($"New Balance: {account.Balance - withdrawn_amount}");
+                account.Balance = account.Balance - withdrawn_amount;
+            }
+        }
+    }
+}
